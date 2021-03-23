2dc18d8671b19e08aa307d3dd67f6e8f9e5a1643
Seongho Jeon <satosily@gmail.com>
2021-03-23 11:37:51 +0900

    이름 주석 및 func3 추가

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+
+int func3()
+{
+	int num = 0;
+	cout << '\n'
+		 << "insert number" << endl;
+	cin >> num;
+	num % 2 == 0;
+	if(num == 0)
+		cout << num << " is even" << endl;
+	else
+		cout << num << " is odd" << endl;
+	
+	return 0;
+}
+
+int func10()
+{
+
+}
+
 int main()
 {
 	while (true)
@@ -10,31 +31,38 @@ int main()
 		cout << '\n'
 			 << "Select Method below";
 		cout << '\n'
-			 << "1. Swap two number 황현성";
+			 << "1. Swap two number";// 황현성
 		cout << '\n'
-			 << "2. Quotient and Remainder 박재성";
+			 << "2. Quotient and Remainder";//  박재성
 		cout << '\n'
-			 << "3. Check whether number is Even or Odd 전성호";
+			 << "3. Check whether number is Even or Odd";//  전성호
 		cout << '\n'
-			 << "4. Calculate Sum of Natural Numbers 김은경";
+			 << "4. Calculate Sum of Natural Numbers";//  김은경
 		cout << '\n'
-			 << "5. Check Leap Year 김량훈";
+			 << "5. Check Leap Year";//  김량훈
 		cout << '\n'
-			 << "6. Factorial 김주성";
+			 << "6. Factorial";//  김주성
 		cout << '\n'
-			 << "7. Find Largest element of an array 김주성";
+			 << "7. Find Largest element of an array";//  김주성
 		cout << '\n'
-			 << "8. Check Prime Number 김량훈";
+			 << "8. Check Prime Number";//  김량훈
 		cout << '\n'
-			 << "9. Display Prime Number between two intervals 김은경";
+			 << "9. Display Prime Number between two intervals";//  김은경
 		cout << '\n'
-			 << "10. Calculate Power of a Number 전성호";
+			 << "10. Calculate Power of a Number";//  전성호
 		cout << '\n'
-			 << "11. Find GCD 박재성";
+			 << "11. Find GCD";//  박재성
 		cout << '\n'
-			 << "12. Find LCM 황현성";
+			 << "12. Find LCM";//  황현성
 		cout << '\n'
 			 << "Select Number>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+
+		cin >> nInputManu;
+
+		if(nInputManu == 3)
+			func3();
+		else 
+			cout << "error" << endl;
 	}
 
 	return 0;
